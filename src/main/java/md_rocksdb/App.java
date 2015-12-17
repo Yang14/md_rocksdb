613acd22c1eaa9c835367938ecb0243b8591ad64
littleyang <359097729@qq.com>
2015-12-17 13:28:45 +0800

    test git & linux env can work on rocksdb.

---
+++ src/main/java/md_rocksdb/App.java
@@ -0,0 +1,25 @@
+package md_rocksdb;
+
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        RocksDB.loadLibrary();
+        // the Options class contains a set of configurable DB options
+        // that determines the behavior of a database.
+        Options options = new Options().setCreateIfMissing(true);
+        RocksDB db = null;
+        try {
+            db = RocksDB.open(options, "F:\\db\\data");
+            System.out.println("Hello World!");
+        } catch (RocksDBException e) {
+        }
+    }
+}
